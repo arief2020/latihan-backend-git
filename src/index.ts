e9f8d2cb1d56b13b6e478375ba834a9eee94ff30
arief2020 <msyaifullahalarief@gmail.com>
2024-07-30 11:39:57 +0700

    Add user authentication endpoints

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express, {Request, Response} from 'express'
+
+const app = express()
+const port = 3000;
+
+app.use(express.json());
+
+// Endpoint for user register
+app.post('/register', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logic User Register
+    res.send('User registered');
+});
+
+// Endpoint user Login
+app.post('/login', (req: Request, res: Response) => {
+    const { username, password } = req.body;
+    // Logic user login
+    res.send('User logged in');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
